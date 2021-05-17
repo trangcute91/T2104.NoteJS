b756a6a9acee6ddb2469d30d4ac3eb42fd79d849
quang anh <crowsstone@gmail.com>
2021-05-18 01:28:17 +0700

    em nộp bài tập

---
+++ programsssssss/baitap3.2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	float y1,y2,y3,y4;
+	y1=4000/100*8+4000;
+	y2=y1/100*8+y1;
+	y3=y2/100*8+y2;
+	y4=y3/100*8+y3;
+	printf("lai suat sau 4 nam an khong ngoi roi la: %f",y4);
+}
