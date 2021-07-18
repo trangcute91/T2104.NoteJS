16765e995099cb643a5a596a00285033106ad111
ThanhVu121198 <Thanhvu121198@gmail.com>
2021-07-18 10:18:17 +0700

    thanh

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-// console.log('Hello world');
-// setInterval(function () {
-//     console.log('hello');
-// },2000);s
+var express = require("express");
+var app = express();
+var port = 5000;
+app.listen(port,function (){
+    console.log("sever is running....");
+});
+app.use(express.static("public"));
+
+app.get("/home",function (req,res) {
+    res.sendFile(__dirname+"/public/....")
+});
+
+app.get("/ke-toan",function (req,res) {
+    res.sendFile(__dirname+"/public/....")
+});
